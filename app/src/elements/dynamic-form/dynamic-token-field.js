28d335a4f54d75217a11b87bf35bacbc7d5c2647
Maxime Mennechet <maxime.mennechet@axel.be>
2020-01-11 22:11:25 +0100

    Fixed tk token field

--- app/src/elements/dynamic-form/dynamic-token-field.js
+++ app/src/elements/dynamic-form/dynamic-token-field.js
@@ -1,5 +1,6 @@
 import './dynamic-link.js';
 import '../../styles/tk-token-field-style.js';
+import '@taktik/tk-token-field/tk-token-field';
 
 import {PolymerElement, html} from '@polymer/polymer';
 import {TkLocalizerMixin} from "../tk-localizer";
